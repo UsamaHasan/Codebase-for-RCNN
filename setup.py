809974d8b8efa7e0f7c3bb6468beee1b4da74814
UsamaHasan <usamahasan72@gmail.com>
2020-10-12 13:48:10 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Code-Base for RCNN-UsamaHasan", # Replace with your own username
+    version="0.0.1",
+    author="Usama Hasan",
+    author_email="usamahasan72@gmail.com",
+    description="A Code Base to implement Regional Conv Networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pucit-waqar/Codebase-of-RCNN",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+

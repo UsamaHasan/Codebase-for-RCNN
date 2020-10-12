ab4db23ae6b04f35a20087bf4292538e8b0a2bf2
Usama Hasan <usamahasan72@gmail.com>
2020-10-12 13:50:35 +0500

    Update setup.py
    fixed the name of the package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Code-Base for RCNN-UsamaHasan", # Replace with your own username
+    name="cbrcnn", # Replace with your own username
     version="0.0.1",
     author="Usama Hasan",
     author_email="usamahasan72@gmail.com",

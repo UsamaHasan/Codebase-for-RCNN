dc5a156defb6e877bb56521140acb70cf296dbad
UsamaHasan <usamahasan72@gmail.com>
2020-10-15 17:40:52 +0500

    Added average precision calculation metrics

--- detection/utils/evaluation.py
+++ detection/utils/evaluation.py
@@ -5,12 +5,108 @@ import torch
 import numpy as np
 
 
-def average_precision(iou,threshold,):
+def compute_ap(recall, precision):
+    """ Compute the average precision, given the recall and precision curves.
+    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+    # Arguments
+        recall:    The recall curve (list).
+        precision: The precision curve (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+    # correct AP calculation
+    # first append sentinel values at the end
+    mrec = np.concatenate(([0.0], recall, [1.0]))
+    mpre = np.concatenate(([0.0], precision, [0.0]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
+
+
+def average_precision(ious,recall,precision,):
     """
     Calculates Minimum Average Precision.
     Average Precision (AP) is finding the area under the precision-recall curve.
     Implement For Pascal VOC 2007 , VOC 2012, and COCO Dataset.
     Formula:
-
     """
-    pass
+    # % Ap at IOU=0.50
+    # Smooth the zigzag curve.
+    i = 0
+    while(i < len(precision)):
+        if precision[i] < precision[i+1]:
+            precision[i] = precision[i+1]
+    recall_ = np.linspace(0,1,11)
+    #Consider them n rows and 2 cols matrix.
+    np.where()
+
+# 11-point interpolated average precision
+def ElevenPointInterpolatedAP(rec, prec):
+    # def CalculateAveragePrecision2(rec, prec):
+    mrec = []
+    # mrec.append(0)
+    [mrec.append(e) for e in rec]
+    # mrec.append(1)
+    mpre = []
+    # mpre.append(0)
+    [mpre.append(e) for e in prec]
+    # mpre.append(0)
+    recallValues = np.linspace(0, 1, 11)
+    recallValues = list(recallValues[::-1])
+    rhoInterp = []
+    recallValid = []
+    # For each recallValues (0, 0.1, 0.2, ... , 1)
+   
+    for r in recallValues:
+        # Obtain all recall values higher or equal than r
+        argGreaterRecalls = np.argwhere(mrec[:] >= r)
+        pmax = 0
+        # If there are recalls above r
+        if argGreaterRecalls.size != 0:
+            pmax = max(mpre[argGreaterRecalls.min():])
+            recallValid.append(r)
+        rhoInterp.append(pmax)
+    
+    # By definition AP = sum(max(precision whose recall is above r))/11
+    ap = sum(rhoInterp) / 11
+    # Generating values for the plot
+    rvals = []
+    rvals.append(recallValid[0])
+    [rvals.append(e) for e in recallValid]
+    rvals.append(0)
+    pvals = []
+    pvals.append(0)
+    [pvals.append(e) for e in rhoInterp]
+    pvals.append(0)
+    # rhoInterp = rhoInterp[::-1]
+    cc = []
+    for i in range(len(rvals)):
+        p = (rvals[i], pvals[i - 1])
+        if p not in cc:
+            cc.append(p)
+        p = (rvals[i], pvals[i])
+        if p not in cc:
+            cc.append(p)
+    recallValues = [i[0] for i in cc]
+    rhoInterp = [i[1] for i in cc]
+    return [ap, rhoInterp, recallValues, None]
+
+
+
+if __name__ ==  '__main__':
+    recall = np.linspace(0,1,5)
+    precision = [0.1,0.2,0.3,0.4,0.6,0.7]
+    print(f'Recall:{recall}')
+    print(f'Precision:{precision}')
+    eval = ElevenPointInterpolatedAP(recall,precision)
+    print(eval)
+    

8fd3f1c79aaf3ffdc660107e36fb0bc655aaea14
UsamaHasan <usamahasan72@gmail.com>
2020-11-26 10:32:39 +0500

    Resolving merge conflicts

--- detection/apis/inference.py
+++ detection/apis/inference.py
@@ -60,12 +60,7 @@ def inference_detector(detector,img):
                 img = torch.from_numpy(img).float().to(device)
             #check for channel first.
             if img.size(0) not in [3,2,1]:
-<<<<<<< HEAD
-                #make channel first.
-                
-=======
                 #make channel first .
->>>>>>> cf55a6ab110297df34c63789d58eec8f1f4b5d2b
                 img = img.permute(2,0,1)
             
             if img.size(1) != detector.input_height  or img.size(2) != detector.input_width:
@@ -79,14 +74,9 @@ def inference_detector(detector,img):
             with torch.no_grad():
                 detections = detector(img)
                 #Apply non-max suppression
-<<<<<<< HEAD
-                #bbox = non_max_suppression(detections,CONFIDENCE_THRESHOLD,NMS_THRESHOLD)
-                bbox = nms(detections,0.5,0.4)
-=======
                 
                 bbox = non_max_suppression(detections,CONFIDENCE_THRESHOLD,NMS_THRESHOLD)
                 
->>>>>>> cf55a6ab110297df34c63789d58eec8f1f4b5d2b
                 if None in bbox:
                     return None 
                 else:
@@ -100,16 +90,6 @@ def inference_detector(detector,img):
 
 # for unit testing.
 if __name__ == '__main__':
-<<<<<<< HEAD
-   
-    detector = init_detector('/home/ncai/Projects/ApiTesting/yolov3.cfg',\
-        checkpoint='/home/ncai/Downloads/yolov3-wider_16000.weights')
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    #input = torch.randn((3,416,416),device=device)
-    img = Image.open('/home/ncai/Downloads/5ea17dd2f12cd750c6b55bdc_simulador-p-800.jpeg')
-    img = img.resize((416,416))
-    out   = inference_detector(detector,img)
-=======
     
     detector = init_detector('/home/ncai01/Downloads/yolov3.cfg',\
         checkpoint='/home/ncai01/Downloads/yolov3-wider_16000.weights')
@@ -119,4 +99,3 @@ if __name__ == '__main__':
     img = img.resize((416,416))
     out   = inference_detector(detector,img)
     
->>>>>>> cf55a6ab110297df34c63789d58eec8f1f4b5d2b
